3cf16110b1c7f2d2cb9dca6c76af75b8740baa63
SMART24\User <dhakla3@gmail.com>
2025-03-31 14:12:02 +0900

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:first/button_page.dart';
 import 'package:first/drawer.dart';
 import 'package:first/flex_page.dart';
 import 'package:first/image_page.dart';
+import 'package:first/layout_page.dart';
 import 'package:first/popup_page.dart';
 import 'package:first/setting_page_dart.dart';
 import 'package:flutter/material.dart';
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: "/",
       routes: {
-        "/": (context) => const MyHomePage(),
+        "/": (context) => const LayoutPage(),
         "/setting": (context) => const SettingPage(),
         "/popup": (context) => const PopupPage(),
         "/flex": (context) => const FlexPage(),
@@ -34,59 +35,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-            title: const Text("안녕하세요")
-        ),
-        drawer: MyDrawer(),
-        body: Container(
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width / 2,
-                    height: 200,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width / 2,
-                    height: 200,
-                    color: Colors.red,
-                  )
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width / 2,
-                    height: 200,
-                    color: Colors.green,
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width / 2,
-                    height: 200,
-                    color: Colors.yellow,
-                  )
-                ],
-              ),
-            ],
-          )
-        )
-    );
-  }
-}
